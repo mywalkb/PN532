28296b8793512acd8a573bd1acb9f82e986f8c69
mywalk <66966897+mywalkb@users.noreply.github.com>
2022-06-12 08:39:26 +0200

    Restore ndef library

--- PN532/examples/emulate_tag_ndef/emulate_tag_ndef.ino
+++ PN532/examples/emulate_tag_ndef/emulate_tag_ndef.ino
@@ -1,4 +1,5 @@
 #include <emulatetag.h>
+#include <NdefMessage.h>
 
 #if 1
 #include <SPI.h>
@@ -16,11 +17,14 @@ EmulateTag nfc(pn532hsu);
 #endif
 
 uint8_t uid[3] = {0x12, 0x34, 0x56};
+NdefMessage message;
+int messageSize;
 
-uint8_t memory_area[] = {
+uint8_t memory_area[160];
+/*= {
   0x00, 0x00, 0x00, 0x00,  // Block 0
   0x00, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0xFF, 0xFF,  // Block 2 (Static lock bytes: CC area and data area are read-only locked)
+  0x00, 0x00, 0x00, 0x00,  // Block 2 (Static lock bytes: CC area and data area are writeable)
   0xE1, 0x10, 0x06, 0x0F,  // Block 3 (CC - NFC-Forum Tag Type 2 version 1.0, Data area (from block 4 to the end) is 48 bytes, Read-only mode)
 
   0x03, 33,   0xd1, 0x02,  // Block 4 (NDEF)
@@ -37,7 +41,10 @@ uint8_t memory_area[] = {
   0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x00,
-};
+};*/
+
+#define PAGESIZE 4
+#define SIZE_USER_DATA sizeof(memory_area) - (4*PAGESIZE) // 4 page for header block 0 - 3
 
 #define READ            0x30
 #define WRITE           0xA2
@@ -46,8 +53,31 @@ uint8_t memory_area[] = {
 
 void setup()
 {
+  memset(memory_area, 0, sizeof(memory_area));
+
+  // init CC to 144 bytes
+  memory_area[0xC] = 0xE1;
+  memory_area[0xD] = 0x10;
+  memory_area[0xE] = 0x12;
+
   Serial.begin(115200);
   Serial.println("------- Emulate Tag --------");
+
+  message = NdefMessage();
+  message.addUriRecord("https://github.com/mywalkb");
+  messageSize = message.getEncodedSize();
+  if (messageSize > SIZE_USER_DATA)
+  {
+    Serial.println("userdata is too small");
+    while (1);
+  }
+
+  Serial.print("Ndef encoded message size: ");
+  Serial.println(messageSize);
+
+  memory_area[0x10] = 3;
+  memory_area[0x11] = messageSize;
+  message.encode(&memory_area[0x12]);
 
   // uid must be 3 bytes!
   nfc.setUid(uid);
@@ -81,11 +111,19 @@ uint8_t processData(const uint8_t *data_in, const size_t data_in_len, uint8_t *d
     switch (data_in[0]) {
       case READ:
         if (data_out_len >= 16) {
-          memcpy(data_out, memory_area + (data_in[1] * 4), 16);
+          if (data_in[1] >= (sizeof(memory_area)/4)) {
+            memset(data_out, 0, 16);
+          } else {
+            memcpy(data_out, memory_area + (data_in[1] * 4), 16);
+          }
           res = 16;
         } else {
           res = -1;
         }
+        break;
+      case WRITE:
+        Serial.print("Write: ");
+        Serial.println(data_in_len);
         break;
       case HALT:
         res = -2;
